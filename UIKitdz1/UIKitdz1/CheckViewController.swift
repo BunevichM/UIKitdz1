95f953f9d35b76464aedd603f7716b2f02159c06
Maxim <57366209+Arhitektormira@users.noreply.github.com>
2022-09-26 18:33:45 +0300

    Сделал 4ое задание с 1го по 5ый урок!

---
+++ UIKitdz1/UIKitdz1/CheckViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CheckViewController.swift
+//  UIKitdz1
+//
+//  Created by MacBook on 26.09.2022.
+//
+
+import UIKit
+/// last VC
+class CheckViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+   
+}
--- UIKitdz1/UIKitdz1/ViewController.swift
+++ UIKitdz1/UIKitdz1/ViewController.swift
@@ -6,10 +6,23 @@
 //
 
 import UIKit
-/// firstVC
+/// firstVC главный экр
 class ViewController: UIViewController {
+    
+    /// Привязываем наши mail и password.
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var enterButton: UIButton!
+    let vc = SecondViewController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func goToSecond(_ sender: UIButton) {
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let vc = storyboard.instantiateViewController(withIdentifier: "SecondViewControllerID")
+                as? SecondViewController else { return }
+        self.present(vc, animated: true)
     }
 }
